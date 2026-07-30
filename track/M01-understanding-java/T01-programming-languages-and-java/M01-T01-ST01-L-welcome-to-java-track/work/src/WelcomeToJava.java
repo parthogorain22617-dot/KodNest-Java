411d3b13887f6719c495e9cdb3158475b30e8a77
Partho <parthogorain22617@gmail.com>
2026-07-30 16:48:45 +0530

    Another Name

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
        System.out.println("Hello");
+       System.out.println("My name is Partho Gorain");
+       System.out.println("My name is Shivam");
     }
 }
