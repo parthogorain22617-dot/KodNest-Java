1e4e485d572918555e23a9663bed156829605adb
Partho <parthogorain22617@gmail.com>
2026-08-04 12:33:58 +0530

    else

---
+++ track/new/Par.java
@@ -0,0 +1,17 @@
+
+
+public class Par {
+    public static void main(String[] args) {
+        int num = 25;
+        if (num>0){
+        System.out.println("positive");}
+        else if (num == 0){
+        System.out.println("0");}
+        else 
+            {System.out.println("negative");}
+            
+        
+        
+    }
+    
+}
