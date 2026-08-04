a126f30f3de7b484f76417bbfe97c75589b054e3
Partho <parthogorain22617@gmail.com>
2026-08-04 11:33:54 +0530

    new

---
+++ track/new/main.java
@@ -0,0 +1,17 @@
+
+// import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+// package project.KodNest-Java.track.new;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("monday");
+       System.out.print("tuesday");
+       System.out.println("wednesday");
+       System.out.println("thrusday");
+       System.out.print("friday");
+       System.out.println("saturday");
+       System.out.println("sunday");
+    }
+    
+}
